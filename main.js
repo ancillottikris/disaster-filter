6a1427c00040e7dc0b9ca8a4d1aa49fe0a500add
Kristen <kristen@MacBook-Pro-2.local>
2019-03-26 19:58:55 -0400

    javascript

--- main.js
+++ main.js
@@ -1,17 +1,42 @@
-$("body").on("click", ".filter", function (e) {
-	e.preventDefault();
+$("body").on("change", "input[type='checkbox']", function (e) {
+	let selection = [];
 
-	console.log("Hello, world!");
+	$("input[type='checkbox']:checked").each(function () {
+		const $this_checkbox = $(this),
+			id = $this_checkbox.attr("id");
 
-	const $this_filter = $(this);
+		selection.push(`.${id}`);
+	});
 
-	const state_name = $this_filter.val();
+	selection = selection.join(",");
+
+	console.log(selection);
 
 	$(".state.active").removeClass("active");
-	$("." + state_name).addClass("active");
+	$(selection).addClass("active");
 
 	console.log("We made it!");
-	console.log("state name" + state_name)
+	// console.log("state name" + state_name)
+});
 
 
+$("body").on("change", "#state", function () {
+	console.log("Change on all checkbox");
+	if ($("#state:checked").length > 0) {
+		console.log("Checkbox is checked");
+		$("input[type='checkbox']:not(#state):checked").each(function () {
+			$(this).prop("checked", false);
+		});
+		// $("input[type='checkbox']:not(#state):checked").trigger("change");
+		console.log("Unchecking all other checkboxes");
+	}
+});
+
+$("body").on("change", "input[type='checkbox']:not(#state)", function () {
+	console.log("Change on other checkboxes");
+	if ($("input[type='checkbox']:not(#state):checked").length > 0) {
+		console.log("There are other checkboxes checked");
+		$("#state:checked").prop("checked", false).trigger("change");
+		console.log("Removed check from all");
+	}
 });
